1199bcb193a17d9130fffb7af44c848a7d4dca7c
Евгений Купряев <evgenyi13v@icloud.com>
2022-04-03 21:56:25 +0300

    Логика игры в отдельном классе

---
+++ Drivers/Logic.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drivers
+{
+    internal class Logic
+    {        
+        public int TicNpc { get; set; }//Период движения НПС
+        public int? cmd { get; set; }//Команды контроллера
+        public bool game { get; set; }//Статус запуска игры
+        public Score scorv { get; set; }//Система счетчика очков
+        public Car user { get; set; }//Модель пользователя
+        public Car[] npclib { get; set; }//Модели НПС
+        public Drow Drow { get; set; }//Класс рисования
+        public List<Car> cars { get; set; }//Общие объекты 
+        public Logic()
+        {
+
+        }
+        public void StartGame()
+        {
+            Task.Run(() =>
+            {
+                while (game)
+                {
+                    switch (cmd)
+                    {
+                        case 0:
+                            Drow.DrowDelete(user);
+                            user.y--;
+                            break;
+                        case 1:
+                            Drow.DrowDelete(user);
+                            user.y++;
+                            break;
+                        case 2:
+                            Drow.DrowDelete(user);
+                            user.x--;
+                            break;
+                        case 3:
+                            Drow.DrowDelete(user);
+                            user.x++;
+                            break;
+                        default:
+                            break;
+                    }
+                    cmd = null;
+                    foreach (var npc in npclib)
+                    {
+                        if (TicNpc > 0)
+                        {
+                            TicNpc = TicNpc - new Random().Next(10, 100);
+                        }
+                        else
+                        {
+                            if (npc.y < 20)
+                            {
+                                Drow.DrowDelete(npc);
+                                npc.y++;
+                            }
+                            else
+                            {
+                                Drow.DrowDelete(npc);
+                                npc.x = new Random().Next(3, 25);
+                                npc.y = 0;
+                            }
+                            TicNpc = 1000;
+                            scorv.ScoreValue++;
+                        }
+                        if (!user.Avaria(npc))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("GameOver");
+                            game = false;
+                            break;
+                        }
+                    }
+                    Console.SetCursorPosition(30, 20);
+                    Console.Write(scorv.Display);
+                    Drow.DrowImage();
+                    Thread.Sleep(10);
+                }
+            });
+        }
+    }
+}
--- Drivers/Program.cs
+++ Drivers/Program.cs
@@ -1,101 +1,31 @@
 ﻿using Drivers;
 
-bool game = true;
-int? cmd = null;
+
 Console.CursorVisible = false;
-for(int i = 0; i < 26; i++)
+Console.WriteLine("Нажмите любую клавишу чтобы начать игру");
+Console.ReadLine();
+var logic = new Logic();
+logic.game = true;
+logic.user = new Car(new Position() { x = 10, y = 10 });
+for (int i = 0; i < 26; i++)
 {
     Console.SetCursorPosition(0, i);
     Console.WriteLine("|");
     Console.SetCursorPosition(26, i);
     Console.WriteLine("|");
 }
-
-//Создаем машинку пользователя
-var user = new Car(new Position() { x = 10, y = 10 });
-var npclib = new Car[5];
-
-//Добавляем хранилище объектов
-var cars = new List<Car>();
-//Добавляем сюда нашу машинку
-cars.Add(user);
-//Добавляем остальные объекты
+logic.npclib = new Car[5];
+logic.cars = new List<Car>();
+logic.cars.Add(logic.user);
 for(int i = 0; i < 5; i++)
 {
-    npclib[i] = new Car(new Position() { x = new Random().Next(0, 25), y = 0 });
-    cars.Add(npclib[i]);
+    logic.npclib[i] = new Car(new Position() { x = new Random().Next(3, 25), y = 0 });
+    logic.cars.Add(logic.npclib[i]);
 }
-
-
-//Система очков
-var scorv = new Score();
-
-//Включаем службу рисования
-var Drow = new Drow(cars);
-//Запускаем все хозяйство
-int TicNpc = 1000;
-Task.Run(() =>
-{
-    while (game)
-    {
-        switch (cmd)
-        {
-            case 0:
-                Drow.DrowDelete(user);
-                user.y--;              
-                break;
-            case 1:
-                Drow.DrowDelete(user);
-                user.y++;               
-                break;
-            case 2:
-                Drow.DrowDelete(user);
-                user.x--;               
-                break;
-            case 3:
-                Drow.DrowDelete(user);
-                user.x++;
-                break;
-            default:
-                break;
-        }
-        cmd = null;
-        foreach(var npc in npclib)
-        {
-            if (TicNpc > 0)
-            {
-                TicNpc = TicNpc - new Random().Next(10, 100);
-            }
-            else
-            {
-                if (npc.y < 20)
-                {
-                    Drow.DrowDelete(npc);
-                    npc.y++;
-                }
-                else
-                {
-                    Drow.DrowDelete(npc);
-                    npc.x = new Random().Next(3, 25);
-                    npc.y = 0;
-                }
-                TicNpc = 1000;
-                scorv.ScoreValue++;
-            }
-            if(!user.Avaria(npc))
-            {
-                Console.Clear();
-                Console.WriteLine("GameOver");
-                game = false;
-                break;
-            }
-        }
-        Console.SetCursorPosition(30, 20);
-        Console.Write(scorv.Display);
-        Drow.DrowImage();
-        Thread.Sleep(10);       
-    }
-});
+logic.scorv = new Score();
+logic.Drow = new Drow(logic.cars);
+logic.TicNpc = 1000;
+logic.StartGame();
 /// <summary>
 /// Контроль клавиш
 /// </summary>
@@ -105,19 +35,19 @@ while(true)
     switch (consoleKey.Key)
     {
         case ConsoleKey.UpArrow:
-            cmd = 0;
+            logic.cmd = 0;
             break;
         case ConsoleKey.DownArrow:
-            cmd = 1;
+            logic.cmd = 1;
             break;
         case ConsoleKey.LeftArrow:
-            cmd = 2;
+            logic.cmd = 2;
             break;
         case ConsoleKey.RightArrow:
-            cmd = 3;
+            logic.cmd = 3;
             break;
         default:
-            cmd = null;
+            logic.cmd = null;
             break;
     }
 }
